d9702abb09a71d492a38b6eab285222338eee6eb
USER <user@USERs-MacBook-Pro.local>
2020-03-05 06:04:06 -0500

    Updates

---
+++ precache-manifest.fa9c97ed497cffbc04ad15db1f7671ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a4e08cc45600fe20860",
+    "url": "/MatchingGame/static/js/runtime~main.8a4e08cc.js"
+  },
+  {
+    "revision": "5a08d630984b2bfce4b7",
+    "url": "/MatchingGame/static/js/main.5a08d630.chunk.js"
+  },
+  {
+    "revision": "6d2248c725aa839ee0a7",
+    "url": "/MatchingGame/static/js/1.6d2248c7.chunk.js"
+  },
+  {
+    "revision": "5a08d630984b2bfce4b7",
+    "url": "/MatchingGame/static/css/main.218e7064.chunk.css"
+  },
+  {
+    "revision": "951a41fd56f2517040c57b3ef394c033",
+    "url": "/MatchingGame/index.html"
+  }
+];
